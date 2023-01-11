73da31391e5e3189d44d363369040563bd80f7cd
unknown <lienapuey@gmail.com>
2023-01-11 22:02:32 +0100

    type error

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-var API_URL = "https://icanhazdadjoke.com/";
-var jokeBtn = document.getElementById("jokeBtn");
-var jokeDiv = document.getElementById("printJoke");
-jokeBtn.onclick = function () {
-    fetch(API_URL, {
-        headers: {
-            Accept: "application/json"
-        }
-    })
-        .then(function (response) { return response.json(); })
-        .then(function (data) {
-        jokeDiv.innerHtml = "<p>".concat(data.joke, "</p>");
-        console.log(data.joke);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const API_URL = "https://icanhazdadjoke.com/";
+const jokeBtn = document.getElementById("jokeBtn");
+const jokeDiv = document.getElementById("joke");
+const headerApi = {
+    headers: {
+        Accept: "application/json",
+    }
+};
+// fetch(API_URL, {
+//     headers: {
+//          Accept: "application/json",
+//        }
+//  })
+//  .then(response => response.json())
+//  .then(data=> { 
+//     jokeDiv.innerHTML=`<p>${data.joke}</p>`;
+//     console.log(data.joke);
+// });
+const showJoke = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const respuesta = yield fetch(API_URL, headerApi);
+        if (respuesta.status === 200) {
+            const data = yield respuesta.json();
+            jokeDiv.innerHTML = data.joke;
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
--- app.ts
+++ app.ts
@@ -1,17 +1,33 @@
 const API_URL: string = "https://icanhazdadjoke.com/"
 const jokeBtn:any = document.getElementById("jokeBtn");
-const jokeDiv :any = document.getElementById("printJoke");
+const jokeDiv: HTMLElement | any = document.getElementById("joke");
+const headerApi = {
+  headers: {
+    Accept: "application/json",
+  }
+}
+   
+// fetch(API_URL, {
+//     headers: {
+//          Accept: "application/json",
+//        }
+//  })
+//  .then(response => response.json())
+//  .then(data=> { 
+//     jokeDiv.innerHTML=`<p>${data.joke}</p>`;
+//     console.log(data.joke);
+// });
 
-jokeBtn.onclick = function() {    
-fetch(API_URL, {
-    headers: {
-         Accept: "application/json",
-       }
- })
- .then(response => response.json())
- .then(data=> { 
-    jokeDiv.innerHtml=`<p>${data.joke}</p>`;
-    console.log(data.joke);
-});
+const showJoke = async() => {
+  try {
+    const respuesta = await fetch(API_URL,headerApi);
 
-  };
+    if (respuesta.status === 200){
+      const data = await respuesta.json();
+      jokeDiv.innerHTML = data.joke;
+    }
+
+  } catch(error) {
+    console.log(error);
+  }
+}
